f0f536301ab623038f95e63aecec0e8326b48ff1
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-11-13 22:08:19 +0700

    create and set splash screen

--- App/index.js
+++ App/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import {StyledContainer} from './styles/globalStyles';
 import {Provider} from 'react-redux';
 import store from './redux/store';
+import SplashScreen from 'react-native-splash-screen';
 
 import Screens from './navigations';
 
 class App extends React.Component {
+  componentDidMount() {
+    // do stuff while splash screen is shown
+    // After having done stuff (such as async tasks) hide the splash screen
+    SplashScreen.hide();
+  }
   render() {
     return (
       <Provider store={store}>
