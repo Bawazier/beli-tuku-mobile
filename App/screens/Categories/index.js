9dc7a21f3390a39b367439922246e7bc81a42283
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-12-16 22:30:15 +0700

    fix categories

--- App/screens/Categories/index.js
+++ App/screens/Categories/index.js
@@ -16,36 +16,18 @@ import {useNavigation} from '@react-navigation/native';
 //Actions
 import HomeActions from '../../redux/actions/home';
 
-const Categories = () => {
-  const categories = useSelector((state) => state.categories);
-  const dispatch = useDispatch();
-  const navigation = useNavigation();
-  const [data, setData] = useState(
-    categories.data.length ? categories.data : null,
+const Categories = ({navigation}) => {
+  const {data, isLoading, isError} = useSelector(
+    (state) => state.listCategories,
   );
-
-  const NextLink = async (info) => {
-    const {count} = categories.pageInfo;
-    console.log(info.distanceFromEnd);
-    if (info.distanceFromEnd >= 0) {
-      if (count > 10) {
-        await dispatch(HomeActions.findCategories(10));
-        setData([...data, ...categories.data]);
-      }
-    }
-  };
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(HomeActions.findCategories());
+    dispatch(HomeActions.listCategories());
   }, []);
 
-  const onPressItem = async (id) => {
-    await dispatch(HomeActions.byCategory(id));
-    navigation.navigate('Catalog');
-  };
-
-  const onPressButton = async () => {
-    await dispatch(HomeActions.search());
+  const searchByCategory = (catalog_name = '') => {
+    dispatch(HomeActions.catalogSearch('', catalog_name));
     navigation.navigate('Catalog');
   };
 
@@ -56,39 +38,34 @@ const Categories = () => {
           block
           rounded
           success
-          style={{elevation: 10}}
-          onPress={() => onPressButton()}>
+          onPress={() => searchByCategory()}
+          style={{elevation: 10, color: '#9ce47c'}}>
           <StyledTextButton>VIEW ALL ITEMS</StyledTextButton>
         </Button>
       </StyledViewButton>
+      <StyledText>Choose category</StyledText>
       <Content>
-        <StyledText>Choose category</StyledText>
-        <FlatList
-          onEndReachedThreshold={0.5}
-          onEndReached={NextLink}
-          data={data}
-          renderItem={({item}) => (
-            <List>
-              <ListItem onPress={() => onPressItem(item.id)}>
-                <StyledTextCategory>{item.name}</StyledTextCategory>
-              </ListItem>
-            </List>
-          )}
-          horizontal={false}
-          keyExtractor={(item) => item.id}
-          // ListFooterComponent={
-          //   <View style={{alignItems: 'center', width: '100%'}}>
-          //     <Spinner color="green" />
-          //   </View>
-          // }
-        />
-        {/* <List>
-          {categories.data.map((item) => (
-            <ListItem onPress={() => onPressItem(item.id)}>
-              <StyledTextCategory>{item.name}</StyledTextCategory>
-            </ListItem>
-          ))}
-        </List> */}
+        {!isLoading && !isError && (
+          <FlatList
+            onEndReachedThreshold={0.5}
+            // onEndReached={NextLink}
+            data={data}
+            renderItem={({item}) => (
+              <List>
+                <ListItem onPress={() => searchByCategory(item.name)}>
+                  <StyledTextCategory>{item.name}</StyledTextCategory>
+                </ListItem>
+              </List>
+            )}
+            horizontal={false}
+            keyExtractor={(item) => item.id}
+            // ListFooterComponent={
+            //   <View style={{alignItems: 'center', width: '100%'}}>
+            //     <Spinner color="green" />
+            //   </View>
+            // }
+          />
+        )}
       </Content>
     </>
   );
